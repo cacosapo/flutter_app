619c1728aa0b0f9f6be5c491ffc4a6163f06cd1d
Lucas <luckanadani@gmail.com>
2019-05-08 18:48:09 -0300

    Unstable version

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,134 +1,143 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:transparencia/blocs/navbar/navbar_bloc.dart';
+import 'package:transparencia/blocs/navbar/navbar_state.dart';
 import 'package:transparencia/models/model.dart';
 import 'package:transparencia/repository/portal_api_provider.dart';
+import 'package:transparencia/ui/bottom_navy_bar.dart';
 
 class MyApp extends StatelessWidget {
-  final PortalApiProvider provider = new PortalApiProvider();
   @override
   Widget build(BuildContext context) {
-    final title = 'Basic List';
-    
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: title,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: Center(
-          child: FutureBuilder<List<Cartao>>(
-            future: provider.getCartao(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return new ListView.builder(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return new Column(
-                      children: <Widget>[
-                        new ListTile(
-                          title: new Text(snapshot.data[index].valorTransacao + " - " + snapshot.data[index].tipoCartaoDescricao),
-                        ),
-                        new Divider(
-                          height: 2.0,
-                        ),
-                      ],
-                    );
-                  },
-                );
-              } else if (snapshot.hasError) {
-                //checks if the response throws an error
-                return Text("${snapshot.error}");
-              }
-              return CircularProgressIndicator();
-            },
-          ),
-        ),
-      ),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class MainPage extends StatelessWidget {
   final String title;
+  final int currentIndex;
 
+  MainPage({@required this.title,@required this.currentIndex});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    if(currentIndex == 1){
+      return FloatingActionButton(
+        child: Icon(Icons.add)
+      );
+    } else 
+    return Text("title" + title);
+  }
+}
+
+class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  NavbarBloc _navbarBloc;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _navbarBloc = NavbarBloc();
+  }
+
+  @override
+  void dispose() {
+    _navbarBloc.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return BlocBuilder(
+      bloc: _navbarBloc,
+      builder: (BuildContext context, NavbarState state) {
+        if (state is ShowBlue)
+          return buildHomepage(state.title, Colors.blue, state.itemIndex);
+        if (state is ShowGreen)
+          return buildHomepage(state.title, Colors.green, state.itemIndex);
+        if (state is ShowRed)
+          return buildHomepage(state.title, Colors.red, state.itemIndex);
+      },
+    );
+  }
+
+  Scaffold buildHomepage(String title, Color color, int currentIndex) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      body: Container(
+        color: color,
+        child: Center(
+          child: MainPage(title: title,currentIndex: currentIndex),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomNavyBar(
+        selectedIndex: currentIndex,
+        onItemSelected: (index) => setState(() {
+              currentIndex = index;
+            }),
+        items: [
+          BottomNavyBarItem(
+            icon: Icon(Icons.apps),
+            title: Text('Home'),
+            activeColor: Colors.red,
+          ),
+          BottomNavyBarItem(
+              icon: Icon(Icons.people),
+              title: Text('Users'),
+              activeColor: Colors.purpleAccent),
+          BottomNavyBarItem(
+              icon: Icon(Icons.message),
+              title: Text('Messages'),
+              activeColor: Colors.pink),
+          BottomNavyBarItem(
+              icon: Icon(Icons.settings),
+              title: Text('Settings'),
+              activeColor: Colors.blue),
+        ],
+      ),
+    );
+  }
+}
+
+class BottomNavCustom extends StatelessWidget {
+  final NavbarBloc navbarBloc;
+  final int currentIndex;
+  final List<BottomNavigationBarItem> options = [
+    BottomNavigationBarItem(
+      icon: Icon(Icons.looks_one),
+      title: Text("Red"),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.looks_two),
+      title: Text("Blue"),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.looks_3),
+      title: Text("Green"),
+    )
+  ];
+
+  BottomNavCustom({this.navbarBloc, this.currentIndex});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index) {
+        if (index == 0) navbarBloc.dispatch(NavbarItems.Red);
+        if (index == 1) navbarBloc.dispatch(NavbarItems.Blue);
+        if (index == 2) navbarBloc.dispatch(NavbarItems.Green);
+      },
+      items: options,
     );
   }
 }
